56a6cefac38e3e6025ad9a73d66ef351c4605541
isaul19 <saulhola348@gmail.com>
2023-12-06 16:25:09 -0500

    update by database

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,81 +1,62 @@
 import { Request, Response } from "express";
-
-interface Todo {
-  id: number;
-  text: string;
-  completedAt: Date;
-}
-
-let todos: Todo[] = [
-  { id: 1, text: "Buy milk", completedAt: new Date() },
-  { id: 2, text: "Buy Bread", completedAt: new Date() },
-  { id: 3, text: "Buy Butter", completedAt: new Date() },
-];
+import { prisma } from "../../data/postgress";
 
 export class TodosController {
-  public static getTodos(req: Request, res: Response) {
-    return res.json(todos);
-  }
+  public static getTodos = async (req: Request, res: Response) => {
+    const todosDb = await prisma.todo.findMany();
 
-  public static getTodoById(req: Request, res: Response) {
+    return res.json(todosDb);
+  };
+
+  public static getTodoById = async (req: Request, res: Response) => {
     const id = Number(req.params.id);
     if (!isFinite(id)) return res.status(400).json({ message: "id is not a number" });
 
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) return res.status(404).json({ message: `todo with id ${id} not found` });
+    const todoDb = await prisma.todo.findFirst({ where: { id: id } });
+    if (!todoDb) return res.status(404).json({ message: `todo with id ${id} not found` });
 
-    return res.json(todo);
-  }
+    return res.json(todoDb);
+  };
 
-  public static createTodo(req: Request, res: Response) {
+  public static createTodo = async (req: Request, res: Response) => {
     const { text } = req.body;
 
-    if (!text || typeof text !== "string")
-      return res.status(400).json({ message: "field 'text' is not valid string" });
-
-    const newTodo = {
-      id: todos.length + 1,
-      text: text,
-      completedAt: new Date(),
-    };
-
-    todos.push(newTodo);
+    await prisma.todo.create({ data: { text: text } });
 
     return res.status(201).json({ message: "todo created success" });
-  }
+  };
 
-  public static updateTodoById(req: Request, res: Response) {
-    const { text } = req.body;
+  public static updateTodoById = async (req: Request, res: Response) => {
+    const { text, completedAt } = req.body;
     const id = Number(req.params.id);
 
     if (!isFinite(id)) return res.status(400).json({ message: "id is not a number" });
 
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) return res.status(404).json({ message: `todo with id ${id} not found` });
+    const todoDb = await prisma.todo.findFirst({ where: { id: id } });
+    if (!todoDb) return res.status(404).json({ message: `todo with id ${id} not found` });
 
-    if (!text || !text.trim())
-      return res.status(400).json({ message: "field 'text' is not valid string" });
-
-    const newTodo = {
-      id: id,
-      text: text,
-      completedAt: new Date(),
-    };
-
-    todos = todos.map((todo) => (todo.id === id ? newTodo : todo));
+    await prisma.todo.update({
+      where: { id: id },
+      data: {
+        text: text || todoDb.text,
+        completedAt: completedAt || todoDb.completedAt,
+      },
+    });
 
     return res.json({ message: "update success" });
-  }
+  };
 
-  public static removeTodoById(req: Request, res: Response) {
+  public static removeTodoById = async (req: Request, res: Response) => {
     const id = Number(req.params.id);
     if (!isFinite(id)) return res.status(400).json({ message: "id is not a number" });
 
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo) return res.status(404).json({ message: `todo with id ${id} not found` });
+    const todoDb = await prisma.todo.findFirst({ where: { id: id } });
+    if (!todoDb) return res.status(404).json({ message: `todo with id ${id} not found` });
 
-    todos = todos.filter((todo) => todo.id !== id);
+    await prisma.todo.delete({
+      where: { id: id },
+    });
 
     return res.json({ message: "remove success" });
-  }
+  };
 }
